8ea16faa541ac9a7219fe4a30ee98ec5a1038159
Jonathan Brachthäuser <jonathan@b-studios.de>
2017-05-19 15:29:08 +0200

    Prepare travis ci

---
+++ scripts/publishMicrosite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+git config --global user.email "jonathan@b-studios.de"
+git config --global user.name "b-studios"
+git config --global push.default simple
+
+sbt docs/publishMicrosite
