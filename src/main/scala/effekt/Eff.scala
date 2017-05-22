a87e0537ca50c2d53b6456cb068a6f13caa254ba
Jonathan Brachthäuser <jonathan@b-studios.de>
2017-05-22 11:51:29 +0200

    Add definition helper functions

--- src/main/scala/effekt/Eff.scala
+++ src/main/scala/effekt/Eff.scala
@@ -6,4 +6,10 @@ trait Eff extends Serializable {
   type @@[A, R] = State => (A => State => Control[Out[R]]) => Control[Out[R]]
 
   def unit[A]: (State, A) => Out[A]
+
+  protected[this] implicit def noState[A, R](f: (A => Control[Out[R]]) => Control[Out[R]] ): A @@ R =
+    s => resume => f(a => resume(a)(s))
+
+  protected[this] implicit def just[A, R](a: A): A @@ R =
+    s => resume => resume(a)(s)
 }
