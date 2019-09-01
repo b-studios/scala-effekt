bf99865958027dd0c201011e5aae8b7728099025
Jonathan Brachthäuser <jonathan@b-studios.de>
2019-09-01 18:02:52 +0200

    Delete travis build configuration for this branch

--- scripts/publishMicrosite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-git config --global user.email "jonathan@b-studios.de"
-git config --global user.name "b-studios"
-git config --global push.default simple
-
-sbt "project docs" publishMicrosite
